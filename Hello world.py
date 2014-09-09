19f744f3168f103318e25f164b83401597ca1580
Christopher14 <40777@PC004148.coll.lr.local>
2014-09-09 13:01:50 +0100

    Hello world 2 second commit

--- Hello world.py
+++ Hello world.py
@@ -2,5 +2,6 @@
 #09.09.2014
 #First exercise Hello World.
 
-print("hello world")
-
+print("hello world") #Print statement 
+first_name=input("please enter your name:") # input gets data from the keyboard will save why you type as (first_name)
+print("Hi {0}!".format(first_name)) # will place first name in {0} 
