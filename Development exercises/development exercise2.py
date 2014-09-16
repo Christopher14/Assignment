c8149b9078ebfd2949a9f42b30e093cbe9e66f32
Christopher14 <40777@PC004148.coll.lr.local>
2014-09-16 12:10:26 +0100

    converting fahrenheit to centigrade

---
+++ Development exercises/development exercise2.py
@@ -0,0 +1,8 @@
+#Chirsotpher Pullen
+#16-9-14
+#development exercise2
+
+print("hello today i will need you to enter the degrees fahrenheit that you wish to display in centigrade")
+fahrenheit=int(input("please enter the degrees fahrenheit that you wish to be converted:"))
+centigrade=(fahrenheit-32)*(5/9)
+print("{0} fahrenheit is equal too {1} centigrade" .format (fahrenheit, centigrade))
